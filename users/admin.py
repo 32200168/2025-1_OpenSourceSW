0e15fddb965301a926095cfd05bed34038cdb6c6
정근학 <jgh122600@gmail.com>
2025-05-26 18:35:10 +0900

    데이터 베이스 저장 기능 구성중

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import CustomUser, Profile, PlayHistory
+from .models import Profile
 
-admin.site.register(CustomUser)
 admin.site.register(Profile)
-admin.site.register(PlayHistory)
